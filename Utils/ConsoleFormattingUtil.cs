d86b1a8d9bbf1550627f6b6a41ed18db4bdca75f
m.gilbrecht <m.gilbrecht@hottgenroth.de>
2019-10-21 09:07:32 +0200

    Util Methods for Forecolor simplification and an offset for WriteToBottomline method

--- Utils/ConsoleFormattingUtil.cs
+++ Utils/ConsoleFormattingUtil.cs
@@ -6,14 +6,18 @@ namespace ConsoleMenu.Utils
 {
 	static class ConsoleFormattingUtil
 	{
-		public static void WriteOnBottomLine(string text)
+		public static void WriteOnBottomLine(string text) => WriteOnBottomLine(text, 0);
+		public static void WriteOnBottomLine(string text, int bottomMargin)
 		{
 			int x = Console.CursorLeft;
 			int y = Console.CursorTop;
-			Console.CursorTop = Console.WindowTop + Console.WindowHeight - 2;
+			Console.CursorTop = Console.WindowTop + Console.WindowHeight - (1 + bottomMargin);
 			Console.Write(text);
 			// Restore previous position
 			Console.SetCursorPosition(x, y);
 		}
+
+		public static void PushColor(ConsoleColor color) => Console.ForegroundColor = color;
+		public static void PopColor() => Console.ForegroundColor = ConsoleColor.White;
 	}
 }
